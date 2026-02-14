9c708ee5e31c1959c7a0ed21129134b4c7f25129
remas-omri <remasomari2006@gmail.com>
2026-02-14 21:15:02 +0300

    Delete Database/database directory

--- Database/database/database.sql
+++
@@ -1,5 +0,0 @@
--- secure_access_system database.sql
--- Creates the database (schema is in schema.sql / tables.sql)
-
-CREATE DATABASE IF NOT EXISTS `secure_access_system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `secure_access_system`;
